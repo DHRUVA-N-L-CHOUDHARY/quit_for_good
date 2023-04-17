7f3b0707ebe382c2951105554103c32ce4240d47
Kranthiii <lci2021053@iiitl.ac.in>
2023-04-17 17:47:40 +0530

    splash screen

--- lib/views/Splash_screen.dart
+++ lib/views/Splash_screen.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:quit_for_good/utils/String.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenheight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage(""))
-        ),
-      ),
-    );
+        body: Container(
+            height: screenheight,
+            width: screenWidth,
+            decoration: const BoxDecoration(
+                image: DecorationImage(image: AssetImage(splash))),
+                child: const Text("QUUIT FOR GOOD",
+                    style: TextStyle(
+                        color: Color.fromRGBO(87, 51, 83, 1),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30.0))));
   }
+
 }
